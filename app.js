5f266c3078281437d3a1358f76bf71bf45690846
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-08-25 01:05:42 +0500

    added js

---
+++ app.js
@@ -0,0 +1,39 @@
+//variable
+//Q # 1
+//var userName ="Soha"
+//alert(userName);
+
+// Q # 2
+//let myName = "SHAZIA"
+//alert(myName);
+
+// Q # 3
+//let message ="Hello world"
+//alert(message);
+
+// Q # 4
+//let nameOfStudent ="soha"
+// let ageOfStudent = "15 years old"
+// let course = "certified mobile application devoloper"
+// alert(nameOfStudent +  ageOfStuden + course);
+
+//Q#5
+// let pizza= "pizza"
+//alert("pizza \n pizz \n piz \n pi \n p");
+
+//Q#6
+// let email = "sohanaveed@gmail.com"
+// alert("my email address is "  + email );
+
+//Q#7
+//  var book = 'A  smarter \n  Way to Learn JavaScript';
+//  alert(" I am trying to learn from the book "+ book);
+
+//Q#8
+// alert("yah! Ican write HTML content through JavaScript")
+
+//Q#9
+// let desing = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(desing);
+
+ 
