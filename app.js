ca53110f3c3bab20b0a3b268b07e6279c56fe949
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-08-25 01:07:47 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
-//variable
-//Q # 1
-//var userName ="Soha"
-//alert(userName);
+//alert
+// Q # 1
+//alert("Welcome to my page");
 
-// Q # 2
-//let myName = "SHAZIA"
-//alert(myName);
+//Q#2
+//alert("Error! please enter a valid password")
 
-// Q # 3
-//let message ="Hello world"
-//alert(message);
-
-// Q # 4
-//let nameOfStudent ="soha"
-// let ageOfStudent = "15 years old"
-// let course = "certified mobile application devoloper"
-// alert(nameOfStudent +  ageOfStuden + course);
+//Q#3
+//alert("Welcome to JS land... \n Happy Coading")
+ 
+//Q#4
+//alert("welcome to js land...")
+//alert("Happy coading! \n prevent this page from creating additinal dialogs")
 
 //Q#5
-// let pizza= "pizza"
-//alert("pizza \n pizz \n piz \n pi \n p");
+//alert("Hello ... I can run JS through my web browser's console")
 
-//Q#6
-// let email = "sohanaveed@gmail.com"
-// alert("my email address is "  + email );
 
-//Q#7
-//  var book = 'A  smarter \n  Way to Learn JavaScript';
-//  alert(" I am trying to learn from the book "+ book);
 
-//Q#8
-// alert("yah! Ican write HTML content through JavaScript")
-
-//Q#9
-// let desing = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-// alert(desing);
-
- 
